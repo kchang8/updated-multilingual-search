bae169881da7eeb9daf03753f45b4b5c7e7e3e4a
kchang8 <kailiec@csu.fullerton.edu>
2021-10-21 23:16:13 -0700

    made server endpoint https

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://Hapiiserver-env.eba-32y8hpri.us-west-1.elasticbeanstalk.com/`
+    baseURL: `https://Hapiiserver-env.eba-32y8hpri.us-west-1.elasticbeanstalk.com/`
     // baseURL: `http://54.193.20.125:8081/`
   })
 }
